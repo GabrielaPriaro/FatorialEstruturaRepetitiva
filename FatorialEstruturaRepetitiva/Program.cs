3cf43b33b5cad1cb830bf83bd5ba9fb05862dde6
Gabriela <gabriela.priaro@gmail.com>
2024-10-29 20:33:13 +0000

    Implementaçao para calcular o fatorial do numero escolhido.

--- FatorialEstruturaRepetitiva/Program.cs
+++ FatorialEstruturaRepetitiva/Program.cs
@@ -1,10 +1,26 @@
-﻿namespace FatorialEstruturaRepetitiva
+﻿//Utilizando estrutura repetitiva For:
+//Problema "fatorial" (adaptado de URI 1153)
+//Fazer um programa para ler um número natural N (valor máximo: 15), e depois calcular e mostrar o fatorial de N. 
+
+namespace FatorealEstruturaRepetitiva
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n, fatorial;
+
+            fatorial = 1;
+
+            Console.Write("Digite o valor de N: ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                fatorial *= i;
+            }
+
+            Console.WriteLine($"FATORIAL = {fatorial}");
         }
     }
 }
